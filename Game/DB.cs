d6d0789decb6b857fb55c4e6200b61ed0cac9efb
Manoth2001 <balendranmanoth2001@gmail.com>
2024-02-16 23:05:05 +0530

    first commit

---
+++ Game/DB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class DB
+    {
+        public SqlConnection DBconnect()
+        {
+            string con_string = "Data Source=DESKTOP-HA1392D;Initial Catalog=Game;Integrated Security=True;Encrypt=False";
+            SqlConnection con = new SqlConnection(con_string);
+            return con;
+        }
+    }
+}
